8cf2083318c95d03cc104441c6309e4e66727368
Raj Kumar Phagami <magar.fagami@gmail.com>
2023-02-04 21:01:19 -0500

    Created a first component and imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Heading from "./Heading";
 
-function Heading() {
-  return (
-    <h1>This is an heading 1</h1>
-  )
-}
+//infile component
+// function Heading() {
+//   return (
+//     <h1>This is an heading 1</h1>
+//   )
+// }
 
 function App() {
   return (
